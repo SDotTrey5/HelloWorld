523694cf903474b3716ff10e1bf94eed3db06f11
Shawn <SDotTrey5@gmail.com>
2014-10-17 14:59:21 -0400

    Create UMUC_Hello.java
    UMUC version of Hello World

---
+++ UMUC_Hello.java
@@ -0,0 +1,11 @@
+// File:	UMUC_Hello.java
+// Author:	Shawn S.
+// Date: 05-13-2013
+// Purpose: First Java program, test installation
+
+public class UMUC_Hello {
+	
+	public static void main(String[] args) {
+		System.out.println ("We have made contact!");
+	} // end method main
+} // end class UMUC_Hello
